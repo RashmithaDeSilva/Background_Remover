f9c8cf839e65df82c7780efc70405c3170a5e141
Lahiru De Silva <lahiru.de.silva@ifs.com>
2024-04-05 20:34:25 +0530

    Update API

--- Backend/app.py
+++ Backend/app.py
@@ -1,12 +1,31 @@
-from flask import Flask
+from flask import Flask, request, send_file
+import os
+from werkzeug.utils import secure_filename
 
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['POST'])
-def index():
+# Configuration for file upload
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/api/uploadimage', methods=['POST'])
+def background_remover():
+    if request.methods == POST:
+        # Check if the POST request has the file part
+        if 'file' not in request.files:
+            return 'No file part'
+
     return 'Hello, World!'
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True, port=5000)
